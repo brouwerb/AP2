ccc039a45ce6a9d8114c28c98ada2877e5410bdc
brouwerb <benedictbrouwer@gmail.com>
2022-08-30 16:24:38 +0200

    n

--- VAK/norsaug.py
+++ VAK/norsaug.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
 for col in [2, 6, 9, 12]:
-    p = [kalibrierungsFunktion(j) for j in getAxisEasy(5, col-1, "./VAK/Vak.xls", "nr.4")]
+    pv = [kalibrierungsFunktion(j) for j in getAxisEasy(5, col-1, "./VAK/Vak.xls", "nr.4")]
     t = uarray(getAxisEasy(5, col, "./VAK/Vak.xls", "nr.4"), 0.1)
-    print(p, t)
+    pl = 1.82e-5
+    v = ufloat(80e-6, 1e-6)
+    s = [(pl*v)/(i*t) for i in pv]
+    print(pv, t, s)
