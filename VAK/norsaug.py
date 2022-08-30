f8b01995cfcb03e4a438479381240e679037247e
brouwerb <benedictbrouwer@gmail.com>
2022-08-30 16:48:21 +0200

    nor

--- VAK/norsaug.py
+++ VAK/norsaug.py
@@ -9,11 +9,14 @@ from uncertainties import ufloat
 from kalib import kalibrierungsFunktion
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
-
+sg = []
 for col in [2, 6, 9, 12]:
-    pv = [kalibrierungsFunktion(j) for j in getAxisEasy(5, col-1, "./VAK/Vak.xls", "nr.4")]
-    t = uarray(getAxisEasy(5, col, "./VAK/Vak.xls", "nr.4"), 0.1)
-    pl = 1.82e-5
+    pv = [kalibrierungsFunktion(j)*1e2 for j in getAxisEasy(5, col-1, "./VAK/Vak.xls", "nr.4")]
+    t = uarray(getAxisEasy(5, col, "./VAK/Vak.xls", "nr.4"), 1).tolist()
+    print(t)
+    pl = 957e2
     v = ufloat(80e-6, 1e-6)
-    s = [(pl*v)/(i*t) for i in pv]
-    print(pv, t, s)
+    s = [(pl*v)/(pv[i]*t[i])*360 for i in range(len(pv))]
+    sg.append(round_errtex(gewichteterMittelwert([nominal_values(i) for i in s], [std_devs(i) for i in s]), intExtFehler([nominal_values(i) for i in s], [std_devs(i) for i in s])))
+    
+print(sg)
