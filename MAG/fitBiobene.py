a43b4b3c017cfdce333629ffac540f3ba751f415
brouwerb <benedictbrouwer@gmail.com>
2022-08-31 20:24:43 +0200

    fitbiobene

--- MAG/fitBiobene.py
+++ MAG/fitBiobene.py
@@ -19,38 +19,37 @@ SAVE_AS = "./MAG/fit1.pdf"
 
 
 
-def theo_kurve (x,I,R,c):
+def theo_kurve (x,I,R):
     mur = 1
     mu0 = 1.256e-6
     N = 1200
-    return mur * mu0 * N / (2 * np.power((x)**2+ R**2,(2/3))) * R**2 * I+c
+    return mur * mu0 * N / (2 * np.power((x)**2+ R**2,(3/2))) * R**2 * I
 def arrtheo_kurve(x,a):
-    return theo_kurve(x,a[0],a[1],a[2])
+    return theo_kurve(x,a[0],a[1])
 
 
 data = [[getAxisEasy(6,4+(j*3+i),"./MAG/Mag.xls","MAG") for i in range(3)] for j in range(5)]
 dataWithErr = []
-scaler = 1
+scaler = 0.001
 for i in range(len(data)):
     dataWithErr.append([])
-    
     dataWithErr[i].append(arrToUnumpyf([j*scaler for j in data[i][0]],[j*scaler*0.0025+digitalErr(0.5*scaler) for j in data[i][0]]))
     dataWithErr[i].append(arrToUnumpyf([j*scaler for j in data[i][1]],[np.sqrt((j*0.002*scaler)**2+(j*0.003*scaler)**2)+digitalErr(0.01*scaler) for j in data[i][1]]))
     dataWithErr[i].append(arrToUnumpyf([j*scaler for j in data[i][2]],[np.sqrt((j*0.002*scaler)**2+(j*0.003*scaler)**2)+digitalErr(scaler*scaler) for j in data[i][2]]))
 
-print(dataWithErr[0])
+#print(dataWithErr[0])
 fig, ax = plt.subplots()
 ax.grid()
 difference = [[i[1][j]-i[2][j]  for j in range(len(i[1]))] for i in dataWithErr]
-x_n , y_n = mirrorDataAroundX(5,nominal_values(dataWithErr[0][0]),nominal_values(difference[0]))
-print(x_n,y_n)
+x_n , y_n = mirrorDataAroundX(2*scaler,nominal_values(dataWithErr[0][0]),nominal_values(difference[0]))
+#print(x_n,y_n)
 ax.scatter(x_n,y_n,s=15,linewidths=0.5,zorder=10,color = COLOR_STYLE[0],marker="o")
 #ax.errorbar(x_n,y_n,fmt="none",yerr=std_devs(difference[0]),xerr=std_devs(dataWithErr[0][0]),ecolor='black',elinewidth=0.8,capsize=2,capthick=0.8)
 
-vals, errs = optimize.curve_fit(theo_kurve,x_n[14:],y_n[14:],bounds=[[0,0,-np.inf],[2000,np.inf,np.inf]])
+vals, errs = optimize.curve_fit(theo_kurve,x_n[12:],y_n[12:],bounds=[[0,0.01],[1.2,0.2]], method="dogbox")
+print(vals)
 
-
-plot = genDataFromFunktion(1000,-250,250,vals,arrtheo_kurve)
+plot = genDataFromFunktion(1000,-250*scaler,250*scaler,vals,arrtheo_kurve)
 
 ax.plot(plot[0],plot[1])
 
